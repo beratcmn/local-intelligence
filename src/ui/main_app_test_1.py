5b17bd773206c57a30d0bbd22702d170236c64a5
Berat Çimen <beratcmn@hotmail.com>
2024-07-01 19:18:51 +0300

    somewhat working

--- src/ui/main_app_test_1.py
+++ src/ui/main_app_test_1.py
@@ -1,24 +1,42 @@
 import flet as ft
+from pathlib import Path
+import pyautogui
+import keyboard
 
 
 def main(page: ft.Page):
     width = 300
     height = 300
-    page.window.width = width
-    page.window.height = height
+    page.window_width = width
+    page.window_height = height
     page.bgcolor = "transparent"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.MainAxisAlignment.CENTER
-    page.window.title_bar_hidden = True
-    page.window.frameless = True
-    page.window.bgcolor = "transparent"
-    page.window.resizable = False
-    page.window.focused = True
-    page.window.always_on_top = True
-    page.window.left = 1920 / 2 - width / 2
-    page.window.top = 1080 / 2 - height / 2
+    page.window_title_bar_hidden = True
+    page.window_frameless = True
+    page.window_bgcolor = "transparent"
+    page.window_resizable = False
+    page.window_focused = True
+    page.window_always_on_top = True
+    x, y = pyautogui.position()
+    page.window_left = x - width / 2 + 100
+    page.window_top = y - height / 2
+    # page.window_skip_task_bar = True
+    page.window_visible = False
 
     button_colors = ["#ED7D3A", "#1E90FF", "#0CCE6B", "#DCED31", "#EF2D56"]
+
+    def toggle_window():
+        print("Changing visibility")
+        if page.window_visible:
+            page.window_visible = False
+            page.update()
+        else:
+            page.window_visible = True
+            x, y = pyautogui.position()
+            page.window_left = x - width / 2 + 100
+            page.window_top = y - height / 2
+            page.update()
 
     def hover_zoom_effect(e):
         if e.data == "true":
@@ -36,6 +54,8 @@ def main(page: ft.Page):
 
         page.update()
 
+    keyboard.add_hotkey("ctrl+space", toggle_window)
+
     page.add(
         ft.Container(
             # alignment="center",
@@ -44,7 +64,12 @@ def main(page: ft.Page):
                     # ? Icon of the app
                     ft.WindowDragArea(
                         content=ft.Image(
-                            "./assets/sparkles_72x72.png", width=72, height=72
+                            (
+                                Path(__file__).parent.parent.parent
+                                / "assets/sparkles_72x72.png"
+                            ),
+                            width=72,
+                            height=72,
                         ),
                     ),
                     # ? Column of Buttons for the app: Summarize, Compose Mail, Fix Grammar, Extract Keywords, Explain tasks.
